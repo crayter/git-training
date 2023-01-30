230a3b34cb7ef5ac6a461883902916b79b8aa0d0
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 15:53:12 +0200

    5th commit

--- git-training/2nd gittr.php
+++ git-training/2nd gittr.php
@@ -2,12 +2,13 @@
 
 define("CONST_1", "value 1");
 
-echo CONST_1; 
+echo CONST_1,"<br>";
 
 const CONST_2 = 'value 2';
 
-var_dump(CONST_2);
+var_dump(CONST_2,"<br>");
 
 const NAME = 'John';
 
 echo "Hello, {NAME}";
+
