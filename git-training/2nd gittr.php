f01dccb5976d4f46178bca66598cae380217a13a
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 15:57:39 +0200

    6th commit

--- git-training/2nd gittr.php
+++ git-training/2nd gittr.php
@@ -2,13 +2,23 @@
 
 define("CONST_1", "value 1");
 
-echo CONST_1,"<br>";
+echo CONST_1,'<br>';
 
 const CONST_2 = 'value 2';
 
-var_dump(CONST_2,"<br>");
+var_dump(CONST_2,'<br>');
 
 const NAME = 'John';
 
-echo "Hello, {NAME}";
+echo "Hello, {NAME}<br>";
 
+echo '<br>';
+
+$bool = true;
+$bool2 = false;
+
+var_dump($bool2,'<br>');
+
+echo $bool2,'<br>';
+
+var_dump((boolean)0, (bool)'', (bool)123, (bool)'Hello<br>');
