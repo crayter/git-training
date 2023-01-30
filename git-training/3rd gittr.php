010e663277d1c32f68cef4e3a3899add5f5a0250
crayter <97885267+crayter@users.noreply.github.com>
2023-01-30 16:03:22 +0200

    9th commit

--- git-training/3rd gittr.php
+++ git-training/3rd gittr.php
@@ -2,10 +2,23 @@
 
 $a = 2 + (3 * 5);
 var_dump($a);
-
+echo '<br>';
 $b = 20;
 $b = $a + 3;
 $a += 3;
 $a %= 3;
 
 var_dump($a);
+echo '<br>';
+var_dump(3 == 3);
+echo '<br>';
+var_dump(3 === '3');
+echo '<br>';
+var_dump(2 != 3);
+echo '<br>';
+var_dump(2 <> 3);
+echo '<br>';
+var_dump('3' !== '3');
+echo '<br>';
+var_dump(3 !== '3');
+echo '<br>';
